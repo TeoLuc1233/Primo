2791c3c5607d5133e38d8c8f5fd3423634a47d16
Matteo Lucarelli <matteolucarelli89@gmail.com>
2025-07-14 16:29:12 +0200

    v0.0.1_Test

--- src/components/blurTitle.jsx
+++ src/components/blurTitle.jsx
@@ -7,8 +7,9 @@ export default function BlurTitle({ title }) {
   return (
     <motion.h1
       initial={{ filter: 'blur(10px)', opacity: 0 }}
-      whileInView={{ filter: 'blur(0px)', opacity: 1 }}
+      whileInView={{ filter: 'blur(0px)', opacity: 5 }}
       transition={{ duration: 1 }}
+      style={{ fontFamily: "'Arial Black', Arial, sans-serif" }}
     >
       {title}
     </motion.h1>
